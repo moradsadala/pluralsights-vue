7d020906bdba97076f5bb283781ef3f32788c9f1
morad <morad.sadala@gmail.com>
2019-03-19 08:54:58 +0200

    3/19

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var growler = new Vue ({
     el:'#growler',
     data:{
-        appName:'Growler'
+        appName:'growler',
+        appLink:'<a href="#">Growler</a>',
+        // appNameColor: '#777',
+        // appNameFontFamily:'Lucida Sans Unicode'
+        // accentColor:{
+        //     color:'#777'
+        // },
+        // headerStyle:{
+        //     fontFamily:'Lucida Sans Unicode',
+        //     margin:'20px'
+        // }
+        accentColor: 'accent-color',
+        headerStyle: 'header-style',
+        isHeader:false,
     }
 });
-console.log(this.searchService);
